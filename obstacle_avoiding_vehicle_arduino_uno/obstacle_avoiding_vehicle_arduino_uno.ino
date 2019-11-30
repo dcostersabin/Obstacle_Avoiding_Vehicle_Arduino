30c65e49f002f0c9f746e56bb5793f3ff84d36ef
Sabin Dcoster <sabin.dcoster@gmail.com>
2019-11-30 13:57:30 +0545

    Final ProtoType

--- obstacle_avoiding_vehicle_arduino_uno/obstacle_avoiding_vehicle_arduino_uno.ino
+++ obstacle_avoiding_vehicle_arduino_uno/obstacle_avoiding_vehicle_arduino_uno.ino
@@ -6,7 +6,7 @@ Servo servo;
 int trigPin = 14;
 int echoPin = 15;
 long current_center_distance,current_left_distance,current_right_distance;
-int right_angle,left_angle,center_angle,current_angle,vehicle_speed;
+int right_angle,left_angle,center_angle,current_angle,vehicle_speed,check_distance;
 
 
 // initialized two variables for distance and the angle current angle is set to 90 
@@ -19,12 +19,16 @@ void setup()
   left_angle = 135;
   center_angle = 90;
 
+
+  // distance to be checked for
+  check_distance = 30 ;
+
   // initialize pins 
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
   // initialize vehicle speed
-  vehicle_speed = 200 ;
+  vehicle_speed = 100 ;
 
   // attach servo motors
   servo.attach(9);
@@ -34,17 +38,134 @@ void setup()
   motor.setSpeed(vehicle_speed);
   motor.run(RELEASE);
   initial_check();
-  
-  
-  
-   
+
+  if(current_center_distance < check_distance || current_left_distance < check_distance || current_right_distance < check_distance )
+  {
+
+    current_center_distance = current_left_distance = current_right_distance = 0;
+
+  }
+  else if(current_center_distance > current_right_distance && current_center_distance > current_left_distance && current_center_distance > check_distance)
+  {
+    current_right_distance = current_left_distance = 0 ;
+  }
+  else if( current_left_distance > current_center_distance && current_left_distance > current_right_distance && current_left_distance > check_distance)
+  {
+    current_center_distance = current_right_distance = 0;
+  }
+  else if( current_right_distance > current_center_distance && current_right_distance > current_left_distance && current_right_distance > check_distance )
+  {
+    current_center_distance = current_left_distance = 0 ;
+  }
+
+
+
 }
 
 
 // main code 
 void loop()
 {
-  
+
+  if(current_center_distance == 0 && current_right_distance == 0 && current_left_distance == 0 )
+  {
+    while(current_center_distance < check_distance + 10 && current_right_distance < check_distance + 10 && current_left_distance + 10)
+    {
+      move_backward();
+      hold();
+
+      cal_distance_right();
+      delay(1000);
+      cal_distance_left();
+      delay(1000);
+      cal_distance_center();
+      delay(1000);
+
+    }
+    hold();
+    cal_distance_left();
+    delay(1000);
+    cal_distance_right();
+    delay(1000);
+
+    if( current_left_distance > current_right_distance )
+    {
+      servo_left();
+      motor.run(FORWARD);
+      delay(1000);
+      hold();
+      servo_center();
+      delay(1000);
+    }
+    else if(current_right_distance > current_left_distance)
+    {
+      servo_right();
+      motor.run(FORWARD);
+      delay(1000);
+      hold();
+      servo_center();
+      delay(1000);
+    }
+
+  }
+  else if(current_right_distance == 0 && current_left_distance == 0)
+  {
+    move_forward();
+  }
+  else if (current_center_distance == 0 && current_right_distance ==0 )
+  {
+
+    servo_left();
+    motor.run(FORWARD);
+    delay(1000);
+    hold();
+    initial_check();
+  }
+  else if( current_center_distance == 0 && current_left_distance ==0 )
+  {
+    servo_right();
+    servo_left();
+    motor.run(FORWARD);
+    delay(1000);
+    hold();
+    initial_check();
+    hold();
+    initial_check();
+  }
+
+
+  cal_distance_center();
+
+  if(current_center_distance > check_distance)
+  {
+    current_right_distance = current_left_distance = 0 ;
+  }
+  else
+  {
+    hold();
+    initial_check();
+
+    if(current_center_distance < check_distance || current_left_distance < check_distance || current_right_distance < check_distance )
+    {
+
+      current_center_distance = current_left_distance = current_right_distance = 0;
+
+    }
+    else if(current_center_distance > current_right_distance && current_center_distance > current_left_distance && current_center_distance > check_distance)
+    {
+      current_right_distance = current_left_distance = 0 ;
+    }
+    else if( current_left_distance > current_center_distance && current_left_distance > current_right_distance && current_left_distance > check_distance)
+    {
+      current_center_distance = current_right_distance = 0;
+    }
+    else if( current_right_distance > current_center_distance && current_right_distance > current_left_distance && current_right_distance > check_distance )
+    {
+      current_center_distance = current_left_distance = 0 ;
+    }
+
+  }
+
 
 
 }
@@ -66,31 +187,42 @@ long cal_distance()
   return distance;
 }
 
+// stop the venhicle
+void hold()
+{
+  motor.run(RELEASE);
+  delay(2000);
+}
+
+
 // move the vehicle forward   
 void move_forward()
 {
   Serial.println("Moving Forward");
   motor.run(FORWARD);
+  delay(200);
 }
 // move the vehicle backward
 void move_backward()
 {
   Serial.println("Moving Backward");
   motor.run(BACKWARD);
+  delay(500);
+
 }
 
 // checks the directions before the vehicles operates
 void initial_check()
 {
   Serial.println("initial checking...");
-  
+
   cal_distance_left();
   delay(1000);
   cal_distance_right();
   delay(1000);
   cal_distance_center();
   delay(1000);
-  
+
 
 }
 
@@ -143,6 +275,11 @@ long cal_distance_right()
   servo_right();
   current_right_distance = cal_distance();
 }
+
+
+
+
+
 
 
 
